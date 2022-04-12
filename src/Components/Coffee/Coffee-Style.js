086c7a716bb21e86dd8d6485fed170b114a3a998
Torrin Bates <43557445+TorrinBates@users.noreply.github.com>
2022-04-12 16:48:10 -0400

    Coffee Looking good and fading well

--- src/Components/Coffee/Coffee-Style.js
+++ src/Components/Coffee/Coffee-Style.js
@@ -2,15 +2,15 @@ import styled, { keyframes, css } from "styled-components";
 
 export const Container = styled.div`
     position: absolute;
-    bottom: 10%;
-    left: 6%;
+    bottom: 6%;
+    left: 30%;
     z-index: 3;
 `;
 
 export const Cup = styled.div`
     position: relative;
-    width: 280px;
-    height: 300px;
+    width: 112px;
+    height: 120px;
     background: linear-gradient(to right,#f9f9f9,#d9d9d9);
     border-bottom-left-radius: 45%;
     border-bottom-right-radius: 45%;
@@ -18,20 +18,20 @@ export const Cup = styled.div`
 
 export const Top = styled.div`
     position: absolute;
-    top: -30px;
+    top: -12px;
     left: 0;
     width: 100%;
-    height: 60px;
+    height: 24px;
     background: linear-gradient(to right,#f9f9f9,#d9d9d9);
     border-radius: 50%;
 `;
 
 export const Circle = styled.div`
     position: absolute;
-    top: 5px;
-    left: 10px;
-    width: calc(100% - 20px);
-    height: 50px;
+    top: 2px;
+    left: 4px;
+    width: calc(100% - 8px);
+    height: 20px;
     background: linear-gradient(to left,#f9f9f9,#d9d9d9);
     border-radius: 50%;
     box-sizing: border-box;
@@ -40,32 +40,53 @@ export const Circle = styled.div`
 
 export const Liquid = styled.div`
     position: absolute;
-    top: 20px;
+    top: 8px;
     left: 0;
     width: 100%;
     height: 100%;
-    background: linear-gradient(#c57e65, #e28462);
+    background: linear-gradient(#70543e, #8c694e);
     border-radius: 50%;
 `;
 
 export const Handle = styled.div`
     position: absolute;
-    right: -70px;
-    top: 40px;
-    width: 160px;
-    height: 180px;
-    border: 25px solid #dcdcdc;
-    border-left: 25px solid transparent;
-    border-bottom: 25px solid transparent;
+    right: -26px;
+    top: 16px;
+    width: 38.4px;
+    height: 43.2px;
+    border: 10px solid #dcdcdc;
+    border-left: 10px solid transparent;
+    border-bottom: 10px solid transparent;
     border-radius: 50%;
     transform: rotate(46deg);
 `;
+
+const FadeOut = keyframes`
+    0% { opacity: 1; }
+    100% { opacity: 0; }
+`;
+
+const FadeIn = keyframes`
+    0% { opacity: 0; }
+    100% { opacity: 1; }
+`;
+
+const FadeOutRule = css(
+    ['', ' .7s linear forwards;'],
+    FadeOut
+);
+
+const FadeInRule = css(
+    ['', ' .7s linear backwards;'],
+    FadeIn
+);
 
 export const SteamGroup = styled.div`
     position: relative;
     display: flex;
     z-index: 1;
-    padding: 0 20px;
+    padding: 0 8px;
+    animation: ${props => props.steam ? FadeInRule : FadeOutRule};
 `;
 
 const Steaming = keyframes`
@@ -77,32 +98,32 @@ const Steaming = keyframes`
         opacity: 1;
     }
     50% {
-        transform: translateY(-150px) scale(5);
+        transform: translateY(-40px) scale(3);
     }
     95% {
         opacity: 0;
     }
     100% {
-        transform: translateY(-300px) scale(10);
+        transform: translateY(-90px) scale(6);
     }
 `;
 
-const animationRule = css(
+const SteamingRule = css(
     ['', ' 5s linear infinite;'],
     Steaming
 );
 
 export const Steam = styled.span`
     position: relative;
-    bottom: 50px;
+    bottom: 20px;
     display: block;
-    margin: 0 2px 50px;
-    min-width: 8px;
-    height: 120px;
+    margin: 0 0.8px 20px;
+    min-width: 3.2px;
+    height: 48px;
     background: #fff;
     border-radius: 50%;
-    animation: ${animationRule};
+    animation: ${SteamingRule};
     opacity: 0;
-    filter: blur(8px);
-    animation-delay: ${props => --props.i * -0.5}s;
+    filter: blur(3.2px);
+    animation-delay: ${props => props.i * -0.5}s;
 `;
--- src/Components/Coffee/Coffee.js
+++ src/Components/Coffee/Coffee.js
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Cup, Top, Circle, Liquid, Handle, SteamGroup, Steam } from "./Coffee-Style";
 
 function Coffee() {
     const Order = [1,3,16,5,13,20,6,7,10,8,17,11,12,14,2,9,15,4,19];
+    const [steam, setSteam] = useState(true);
+    const onClick = () => { setSteam(!steam); }
 
     return(
-        <Container>
+        <Container onClick={onClick}>
             <Cup>
                 <Top>
-                    <SteamGroup>
+                    <SteamGroup steam={steam}>
                         {
                             Order.map(i => <Steam i={i}/>)
                         }
