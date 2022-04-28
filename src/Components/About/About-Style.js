c578cd160828cd478a64f7c9fb82aaf8098169b0
Torrin Bates <43557445+TorrinBates@users.noreply.github.com>
2022-04-28 15:31:32 -0400

    About Rotation and responsive working

--- src/Components/About/About-Style.js
+++ src/Components/About/About-Style.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { keyframes, css } from "styled-components";
 
 export const Container = styled.div`
     height: 100vh;
@@ -12,37 +12,54 @@ export const Border = styled.div`
     height: 80%;
     justify-self: center;
     align-self: center;
-    border: .5em solid black;
+    border: .4em solid black;
     border-radius: 1em;
     background-color: transparent;
+    overflow-x: hidden;
+    background-color: #7ca77b;
 `;
 
+const Steaming = keyframes`
+    from { transform: rotate(0deg); }
+    to { transform: rotate(360deg); }
+`;
+
+const SteamingRule = css(
+    ['', ' 5s linear infinite;'],
+    Steaming
+);
+
 export const Circle = styled.div`
-    border: .5em solid black;
+    border: .4em dashed black;
     border-radius: 50%;
     width: 28%;
-    height: auto;
-    padding-top: 28%;
-    margin-left: -14%;
-    margin-top: 3%;
+    aspect-ratio: 1 / 1;
+    margin: 2%;
+    float: left;
+    animation: ${SteamingRule};
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: 1fr;
+`;
+
+export const Test = styled.div`
+    border-radius: 50%;
+    width: 95%;
+    aspect-ratio: 1 / 1;
+    justify-self: center;
+    align-self: center;
     background-image: url(https://torrinbates.github.io/images/profile.jpg);
     background-repeat: no-repeat;
     background-position: center;
     background-size: cover;
-    float: left;
-`;
-
-export const Overflow = styled.div`
-    overflow: hidden;
-    height: 100%;
 `;
 
 export const Title = styled.h1`
     font-size: 4em;
-    margin: 0;
+    margin: 0 2%;
 `;
 
 export const Text = styled.h1`
     font-size: 1.2em;
-    margin: 0;
+    margin: 0 2%;
 `;
--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,11 +1,13 @@
 import React from "react";
-import { Container, Border, Circle, Overflow, Title, Text } from "./About-Style";
+import { Container, Border, Circle, Title, Text, Test } from "./About-Style";
 
 function About() {
     return(
         <Container>
             <Border>
-                <Circle/>
+                <Circle>
+                    <Test></Test>
+                </Circle>
                 <Title>Torrin Bates</Title>
                 <Text>
                     My name is torrin bates and I'm a friend of the thing and thats why I have a lot of fun and love software development.  My name is torrin bates and I'm a friend of the thing and thats why I have a lot of fun and love software development.  My name is torrin bates and I'm a friend of the thing and thats why I have a lot of fun and love software development.  My name is torrin bates and I'm a friend of the thing and thats why I have a lot of fun and love software development.  My name is torrin bates and I'm a friend of the thing and thats why I have a lot of fun and love software development.  My name is torrin bates and I'm a friend of the thing and thats why I have a lot of fun and love software development.  My name is torrin bates and I'm a friend of the thing and thats why I have a lot of fun and love software development.  My name is torrin bates and I'm a friend of the thing and thats why I have a lot of fun and love software development.  My name is torrin bates and I'm a friend of the thing and thats why I have a lot of fun and love software development.
